7fdbaab994eccdd313965d99b156a5e3105dced7
HirLuth <ewenn-brieuc@orange.fr>
2022-03-23 17:19:35 +0100

    Le maïs à réaction est presque bon

---
+++ Space Pancake Unity/Assets/Scripts/MaïsAReaction.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class MaïsAReaction : MonoBehaviour
+{
+    [Header("Player refrence")]
+    [SerializeField] private PlayerControls controls;
+    [SerializeField] private Character character;
+    [SerializeField] private GameObject playerGameObject;
+    [SerializeField] private Rigidbody2D playerRB;
+
+    [Header("Self refrence")]
+    [SerializeField] private SpriteRenderer spriteSelf;
+    [SerializeField] private Rigidbody2D rbSelf;
+    [SerializeField] private float maxSpeedGoingUp;
+    [Header("Variables modifiables")] 
+    [SerializeField] private AnimationCurve courbeAccelerationMonté;
+    [SerializeField] private float timerToExplode;
+    [SerializeField] private float positionSetUpDistanceFromEdge;
+    [SerializeField] private float multiplicatorTimeToGetInPosition;
+    [SerializeField] private float directionnalModificator;
+    [SerializeField] private float jumpOutForceX;
+    [SerializeField] private float jumpOutForceY;
+    
+    [Header("Variables de fonctionnement")]
+    [SerializeField] private bool playerIsAtRange;
+    [SerializeField] private bool isOnTheRide;
+    [SerializeField] private bool launchedWithoutPlayer;
+    [SerializeField] private float timer;
+    [SerializeField] private float stockageGravityScaleJoueur;
+    [SerializeField] private float horizontaleSpeedSide;
+
+
+    [Header("Variable UI")] 
+    [SerializeField] private Color colorNotAtRange;
+    [SerializeField] private Color colorAtRange;
+    
+    private void Awake()
+    {
+        controls = new PlayerControls();
+        playerIsAtRange = false;
+        stockageGravityScaleJoueur = playerRB.gravityScale;
+    }
+    
+    private void OnEnable()
+    {
+        controls.Personnage.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Personnage.Disable();
+    }
+
+    private void Update()
+    {
+        if (playerIsAtRange && controls.Personnage.Serpe.WasPressedThisFrame())
+        {
+            isOnTheRide = true;
+        }
+        if (isOnTheRide)
+        {
+            timer += Time.deltaTime;
+            playerRB.gravityScale = 0;
+            character.noControl = true;
+            
+            if (controls.Personnage.MoveRight.WasPerformedThisFrame())
+            {
+                horizontaleSpeedSide = 1;
+            }
+            else if (controls.Personnage.MoveLeft.WasPerformedThisFrame())
+            {
+                horizontaleSpeedSide = -1;
+            }
+            
+            if (controls.Personnage.Sauter.WasPerformedThisFrame())
+            {
+                playerRB.velocity = new Vector2(horizontaleSpeedSide * jumpOutForceX+1, jumpOutForceY);
+                ReintialiseWhenGetOut();
+                launchedWithoutPlayer = true;
+            }
+            
+            if (playerGameObject.transform.position == transform.position + Vector3.down * positionSetUpDistanceFromEdge)
+            {
+                
+                playerRB.velocity = new Vector2(horizontaleSpeedSide*directionnalModificator, courbeAccelerationMonté.Evaluate(timer/timerToExplode) * maxSpeedGoingUp) ;
+                rbSelf.velocity = new Vector2(horizontaleSpeedSide*directionnalModificator, courbeAccelerationMonté.Evaluate(timer/timerToExplode) * maxSpeedGoingUp) ;
+                if (timer >= timerToExplode)
+                {
+                    ReintialiseWhenGetOut();
+                    Destroy(this.gameObject);
+                }
+            }
+            else if (!launchedWithoutPlayer)
+            {
+                playerGameObject.transform.position = Vector3.Lerp(playerGameObject.transform.position, transform.position + Vector3.down*positionSetUpDistanceFromEdge, timer*multiplicatorTimeToGetInPosition);
+            }
+        }
+
+        if (launchedWithoutPlayer)
+        {
+            timer += Time.deltaTime;
+            rbSelf.velocity = Vector2.up * courbeAccelerationMonté.Evaluate(timer/timerToExplode) * maxSpeedGoingUp;
+            if (timer >= timerToExplode)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    private void ReintialiseWhenGetOut()
+    {
+        playerRB.gravityScale = stockageGravityScaleJoueur;
+        character.noControl = false;
+        isOnTheRide = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        playerIsAtRange = true;
+        spriteSelf.color = colorAtRange;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        playerIsAtRange = false;
+        spriteSelf.color = colorNotAtRange;
+    }
+}
