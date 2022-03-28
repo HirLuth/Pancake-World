f016d9e1ab97ec42ea1a09f7fb15c74b4a676ff5
Meleoro <arthur.perigno@gmail.com>
2022-03-28 11:40:39 +0200

    Score (Ecrire "EventManager.Instance.AddPoints()")

--- Space Pancake Unity/Assets/Scripts/EventManager.cs
+++ Space Pancake Unity/Assets/Scripts/EventManager.cs
@@ -2,15 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class EventManager : MonoBehaviour
 {
+    public static EventManager Instance { get; private set; }
+
     [SerializeField] private GameObject UI;
-    
+
+    private int points;
+    [SerializeField] private TextMeshProUGUI score;
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+
     public void Restart()
     {
         Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         UI.SetActive(false);
+    }
+
+
+    public void AddPoints()
+    {
+        points += 1;
+        score.text = "SCORE : " + points;
     }
 }
