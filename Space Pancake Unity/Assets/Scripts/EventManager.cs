3355e4e9f231396b789ff41ee51f83f2dcdf1631
HirLuth <ewenn-brieuc@orange.fr>
2022-04-09 14:31:09 +0200

    Nouvel modif référence UI

--- Space Pancake Unity/Assets/Scripts/EventManager.cs
+++ Space Pancake Unity/Assets/Scripts/EventManager.cs
@@ -29,6 +29,7 @@ public class EventManager : MonoBehaviour
     private void Start()
     {
         UI = UIPrincipale.Instance.gameObject;
+        score = UIPrincipale.Instance.textScore;
     }
 
 
--- Space Pancake Unity/Assets/Scripts/UIPrincipale.cs
+++ Space Pancake Unity/Assets/Scripts/UIPrincipale.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UIPrincipale : MonoBehaviour
 {
     public static UIPrincipale Instance { get; private set; }
+    public TextMeshProUGUI textScore;
 
     private void Awake()
     {
