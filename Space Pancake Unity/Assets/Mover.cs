5c0e52b1974ea9c5f044285a7d6a838d3c2afcfc
Meleoro <arthur.perigno@gmail.com>
2022-04-25 23:12:49 +0200

    Camera rail 2 : le retour

--- Space Pancake Unity/Assets/Mover.cs
+++ Space Pancake Unity/Assets/Mover.cs
@@ -10,9 +10,13 @@ public class Mover : MonoBehaviour
     private float transition;
     private bool isCompleted;
 
+    public bool go;
+    public float vitesseRail;
+
+
     void Update()
     {
-        if (!isCompleted)
+        if (!isCompleted && go)
             Play();
 
         else
@@ -23,7 +27,7 @@ public class Mover : MonoBehaviour
     {
         CameraMovements.Instance.isOnRail = true;
         
-        transition += Time.deltaTime;
+        transition += Time.deltaTime / vitesseRail;
 
         if (transition > 1)
         {
@@ -36,7 +40,17 @@ public class Mover : MonoBehaviour
             currentSeg--;
         }
 
-        CameraMovements.Instance.transform.position = rail.LinearPosition(currentSeg, transition);
-        CameraMovements.Instance.transform.transform.rotation = rail.Orientation(currentSeg, transition);
+        if(currentSeg < rail.nodes.Length - 1)
+        {
+            CameraMovements.Instance.transform.position = rail.CatmullPosition(currentSeg, transition);
+            CameraMovements.Instance.transform.position = new Vector3(CameraMovements.Instance.transform.position.x, CameraMovements.Instance.transform.position.y, -10);
+            CameraMovements.Instance.transform.transform.rotation = rail.Orientation(currentSeg, transition);
+        }
+        else
+        {
+            isCompleted = true;
+        }
+
+        
     }
 }
--- Space Pancake Unity/Assets/Scripts/Camera/Rail.cs
+++ Space Pancake Unity/Assets/Scripts/Camera/Rail.cs
@@ -7,7 +7,7 @@ using UnityEngine;
  [ExecuteInEditMode]
 public class Rail : MonoBehaviour
 {
-    private Transform[] nodes;
+    public Transform[] nodes;
 
     public static Rail Instance;
 
@@ -26,7 +26,7 @@ public class Rail : MonoBehaviour
         return Vector3.Lerp(p1, p2, ratio);
     }
 
-    public Vector3 CatmullPosition(int seg, float ratio)
+    public Vector2 CatmullPosition(int seg, float ratio)
     {
         Vector3 p1, p2, p3, p4;
 
@@ -53,6 +53,14 @@ public class Rail : MonoBehaviour
             p3 = nodes[seg + 1].position;
             p4 = nodes[seg + 2].position;
         }
+
+        float t2 = ratio * ratio;
+        float t3 = t2 * ratio;
+
+        float x = 0.5f * ((2.0f * p2.x) + (-p1.x + p3.x) * ratio + (2.0f * p1.x - 5.0f * p2.x + 4 * p3.x - p4.x) * t2 + (-p1.x + 3.0f * p2.x - 3.0f * p3.x + p4.x) * t3);
+        float y = 0.5f * ((2.0f * p2.y) + (-p1.y + p3.y) * ratio + (2.0f * p1.y - 5.0f * p2.y + 4 * p3.y - p4.y) * t2 + (-p1.y + 3.0f * p2.y - 3.0f * p3.y + p4.y) * t3);
+
+        return new Vector2(x, y);
     }
 
     public Quaternion Orientation(int seg, float ratio)
@@ -66,7 +74,7 @@ public class Rail : MonoBehaviour
     
     private void OnDrawGizmos()
     {
-        for (int i = 1; i < nodes.Length - 1; i++)
+        for (int i = 0; i < nodes.Length - 1; i++)
         {
             Handles.DrawDottedLine(nodes[i].position, nodes[i + 1].position, 3.0f);
         }
