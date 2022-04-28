c8a99d64eb7ab93e00d61f13da015eb8709d75c6
Meleoro <arthur.perigno@gmail.com>
2022-04-28 12:24:01 +0200

    Légères modifs ralenti

--- Space Pancake Unity/Assets/Scripts/Gestion des Spawns/SpawnPointManagement.cs
+++ Space Pancake Unity/Assets/Scripts/Gestion des Spawns/SpawnPointManagement.cs
@@ -30,7 +30,6 @@ public class SpawnPointManagement : MonoBehaviour
     {
         if (activerCheckpoint)
         {
-            Debug.Log(555);
             spawnPointLocation = newSpawnPointLocation;
             spawnWasModifiedOnce = true;
         }
