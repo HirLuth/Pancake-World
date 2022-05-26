3d2572d81fb99199a422696f4a94d5b1eae149fd
HirLuth <ewenn-brieuc@orange.fr>
2022-05-26 18:20:56 +0200

    Début intégration audio

--- Space Pancake Unity/Assets/Scripts/SOUND DESIGN/AudioManager.cs
+++ Space Pancake Unity/Assets/Scripts/SOUND DESIGN/AudioManager.cs
@@ -1,18 +1,39 @@
+using System.Collections.Generic;
 using UnityEngine.Audio;
 using UnityEngine;
 
 
 public class AudioManager : MonoBehaviour
 {
-    public Sound[] sounds;
+    public List<AudioClip> listClip;
+    public AudioSource mainAudioSource;
+    public AudioSource secondaryAudioSource;
+    
     void Start()
     {
-        
+        mainAudioSource.clip = listClip[0];
+        mainAudioSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (CameraMovements.Instance.isOnRail)
+        {
+            SetSurvivalTheme();
+        }
         
+        Debug.Log(mainAudioSource.isPlaying);
+    }
+
+    public void SetForestAmbiance()
+    {
+        secondaryAudioSource.clip = listClip[2];
+    }
+
+    public void SetSurvivalTheme()
+    {
+        mainAudioSource.Stop();
+        mainAudioSource.PlayOneShot(listClip[1]);
     }
 }
