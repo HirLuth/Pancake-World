10b2d0410a3d5f79e0ab5cce01e7b9f6c665223f
Arthur <arthur.perigno@gmail.com>
2022-06-05 14:22:18 +0200

    Problème barre de volume options réglé

--- Space Pancake Unity/Assets/SetVolume.cs
+++ Space Pancake Unity/Assets/SetVolume.cs
@@ -2,15 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SetVolume : MonoBehaviour
 {
     public AudioMixer mixer;
+    public Slider slider;
+
+    void Start()
+    {
+        slider.value = PlayerPrefs.GetFloat("volume", 1);
+        
+        AudioManager.instance.playermastervolume =  PlayerPrefs.GetFloat("volume", 1);;
+        
+        AudioManager.instance.UpdateVolume();
+    }
 
     public void SetLevel(float sliderValue)
     {
         AudioManager.instance.playermastervolume = sliderValue;
         
         AudioManager.instance.UpdateVolume();
+        
+        PlayerPrefs.SetFloat("volume", sliderValue);
     }
 }
